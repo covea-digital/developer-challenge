c33bf1a1c3e19acbfa78ec57033f3beb4e3138fa
Joshurahrah <joshurahrah@outlook.com>
2021-07-08 10:08:59 +0100

    Changed name of unit test folder to fall in line with my accidently named project...

---
+++ PetInsuranceApiTests/PetInsuranceApiTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PetInsuranceApi;
+using System.Linq;
+
+namespace PetInsuranceApiTests
+{
+    [TestClass]
+    public class QuotesTests
+    {
+        [TestMethod]
+        public void TotalPremiums_PremiumsPresent_ShouldBeTotalOfPremiums()
+        {
+            var quotes = new Quotes();
+            var quoteList = new List<Quote>();
+        }
+    }
+}
